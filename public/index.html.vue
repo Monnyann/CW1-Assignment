a71e43303117beba2e1767aaa9e9f0aec39b887e
Monnyann <37423861+Monnyann@users.noreply.github.com>
2019-11-22 16:35:31 +0100

    first commit

---
+++ public/index.html.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <noscript>
+      <strong
+        >We're sorry but real-world-vue doesn't work properly without JavaScript
+        enabled. Please enable it to continue.</strong
+      >
+    </noscript>
+
+    <div id="app"></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
